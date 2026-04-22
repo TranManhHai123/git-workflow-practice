64a21b05ac344d393095b18295f54862383e0c2c
TranManhHai123 <haitentententen@gmail.com>
2026-04-22 08:47:36 +0700

    feat(calculator): add multiply function

--- calculator.js
+++ calculator.js
@@ -1,7 +1,13 @@
 function add(a, b) {
-return a + b;
+    return a + b;
 }
+
 function subtract(a, b) {
-return a - b;
+    return a - b;
 }
-module.exports = {add, subtract};
+
+function multiply(a, b) {
+    return a * b;
+}
+
+module.exports = {add, subtract, multiply};
